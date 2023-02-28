67e6874a7c5eb18059895231a207eb190a63fb15
tnluser <rahul.kumar245@byjus.com>
2023-02-28 14:45:48 +0530

    First change

---
+++ script.js
@@ -0,0 +1,23 @@
+// Open the popup
+let popup=document.getElementById("popup");
+$(document).ready(function() {
+	setTimeout(function() {
+		openPopup();
+	}, 1000);
+});
+
+function openPopup() {
+	 popup.style.display="block";
+}
+  
+  // Close the popup
+  function closePopup() {
+	 
+	popup.style.display = "none";
+  }
+  
+  // Attach the close button click event listener
+  var closeButton = document.getElementById("close-btn");
+  closeButton.addEventListener("click", closePopup);
+
+  
